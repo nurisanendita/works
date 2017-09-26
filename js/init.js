5655b98aa6beaee46f735015bbc3c28f0047793b
nuritharra <nuri.sanendita@gmail.com>
2017-09-27 02:01:26 +0700

    modals fixed v1

--- js/init.js
+++ js/init.js
@@ -181,29 +181,46 @@
 /*----------------------------------------------------*/
 /*	slideshow
 ------------------------------------------------------*/
- var slideIndex = 1;
- showSlides(slideIndex);
+var sliderObjects = [];
+createSliderObjects();
 
- function plusSlides(n) {
-   showSlides(slideIndex += n);
- }
-
- // function currentSlide(n) {
- //   showSlides(slideIndex = n);
- // }
-
- function showSlides(n) {
-   var i;
-   var slides = document.getElementsByClassName("mySlides");
-  //  var dots = document.getElementsByClassName("dot");
-   if (n > slides.length) {slideIndex = 1}
-   if (n < 1) {slideIndex = slides.length}
-   for (i = 0; i < slides.length; i++) {
-       slides[i].style.display = "none";
+function plusDivs(obj, n) {
+ var parentDiv = $(obj).parent();
+ var matchedDiv;
+ $.each(sliderObjects, function(i, item) {
+   if ($(parentDiv[0]).attr('id') == $(item).attr('id')) {
+     matchedDiv = item;
+     return false;
    }
-  //  for (i = 0; i < dots.length; i++) {
-  //      dots[i].className = dots[i].className.replace(" active", "");
-  //  }
-   slides[slideIndex-1].style.display = "block";
-  //  dots[slideIndex-1].className += " active";
+ });
+ matchedDiv.slideIndex=matchedDiv.slideIndex+n;
+ showDivs(matchedDiv, matchedDiv.slideIndex);
+}
+
+function createSliderObjects() {
+ var sliderDivs = $('.slider');
+ $.each(sliderDivs, function(i, item) {
+   var obj = {};
+   obj.id = $(item).attr('id');
+   obj.divContent = item;
+   obj.slideIndex = 1;
+   obj.slideContents = $(item).find('.mySlides');
+   showDivs(obj, 1);
+   sliderObjects.push(obj);
+ });
+}
+
+function showDivs(divObject, n) {
+ debugger;
+ var i;
+ if (n > divObject.slideContents.length) {
+   divObject.slideIndex = 1
  }
+ if (n < 1) {
+   divObject.slideIndex = divObject.slideContents.length
+ }
+ for (i = 0; i < divObject.slideContents.length; i++) {
+   divObject.slideContents[i].style.display = "none";
+ }
+ divObject.slideContents[divObject.slideIndex - 1].style.display = "block";
+}
